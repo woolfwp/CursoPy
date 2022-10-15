3554b9478ea39579284fc3dcfdca4afb01ce9641
sebasPolar <sebasgomez1099@gmail.com>
2022-10-15 01:37:16 -0500

    Cp1

---
+++ Cp1Input.py
@@ -0,0 +1,7 @@
+""" Solicitud de entradas por teclado
+    redondeo de numero
+"""
+nameProd = input("ingrese nombre del producto ") 
+pesoProd = float(input("ingrese peso del producto "))
+
+print(nameProd, " tiene un peso de ", round(pesoProd, 2) )
